7780c406d1e8e462d123f77e74b089a47750be27
natetravis1 <90298840+natetravis1@users.noreply.github.com>
2022-01-12 23:12:29 -0700

    Add project files.

---
+++ FirstASPNETWebApp/wwwroot/javascript/message.js
@@ -0,0 +1,45 @@
+﻿$("#calcButton").click(function () {
+
+    // gets input for each assignment to calculate
+    let class1 = $("#class1").val();
+    let class2 = $("#class2").val();
+    let class3 = $("#class3").val();
+    let class4 = $("#class4").val();
+    let class5 = $("#class5").val();
+
+    let grade = 0;
+
+    grade = (class1) + (class2) + (class3) + (class4) + (class5)
+
+
+
+    // determines letter grade based on numeric grade
+    if (grade >= 94) {
+        letterGrade = "A"
+    } else if (grade >= 90) {
+        letterGrade = "A-"
+    } else if (grade >= 87) {
+        letterGrade = "B+"
+    } else if (grade >= 84) {
+        letterGrade = "B"
+    } else if (grade >= 80) {
+        letterGrade = "B-"
+    } else if (grade >= 77) {
+        letterGrade = "C+"
+    } else if (grade >= 74) {
+        letterGrade = "C"
+    } else if (grade >= 70) {
+        letterGrade = "C-"
+    } else if (grade >= 67) {
+        letterGrade = "D+"
+    } else if (grade >= 64) {
+        letterGrade = "D"
+    } else if (grade >= 60) {
+        letterGrade = "D-"
+    } else {
+        letterGrade = "E"
+    }
+
+    // prints both numeric and letter grade back to user via alert
+    alert("Your grade is " + grade.toString() + "% (" + letterGrade + ")")
+})
